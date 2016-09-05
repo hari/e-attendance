1e9187d64368960617ce0869978aa80cd19bb827
Hari Lamichhane <onwithhari@gmail.com>
2016-09-05 17:27:03 +0545

    create model

---
+++ Models/Model.php
@@ -0,0 +1,13 @@
+<?php namespace Attendance\Models;
+abstract class Model {
+  /** @var Array The property-value pair of all the attributes of model */
+  protected $instance = [];
+
+  public function getAttribute($prop) {
+    if (array_key_exists($prop, $this->instance)) {
+      return $this->instance[$prop];
+    }
+    return null;
+  }
+  
+}
