0cb74c6241197ba1addadc71a611ee0395762ec4
Hari Lamichhane <onwithhari@gmail.com>
2016-09-05 17:19:56 +0545

    add new dirs and logic for loading namespaced classess

--- autoload.php
+++ autoload.php
@@ -1,11 +1,15 @@
 <?php
 $dirs = [
- 'controllers/',
- 'database/',
- 'models/'
+ 'Controllers/',
+ 'Database/',
+ 'Models/',
+ 'Utils/',
+ 'Utils/Validators/'
 ];
 spl_autoload_register(function($class_name) use ($dirs) {
   foreach ($dirs as $key => $dir) {
+    $parts = explode('\\', $class_name);
+    $class_name = end($parts);
     $file = $dir . $class_name . '.php';
     if (file_exists($file)) {
       require_once $file;
