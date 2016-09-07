ebc6ef9707785adb8d3134a7c0185b5043aad596
Hari Lamichhane <onwithhari@gmail.com>
2016-09-07 21:47:45 +0545

    add get, set, pull methods

--- Utils/MessageBox.php
+++ Utils/MessageBox.php
@@ -1,5 +1,19 @@
 <?php namespace Attendance\Utils;
 
 class MessageBox {
-  
+
+  public static function set($message) {
+    $_SESSION['message'] = $message;
+  }
+
+  public static function get() {
+    return (isset($_SESSION['message'])) ? $_SESSION['message'] : [];
+  }
+
+  public static function pull() {
+    $ret = MessageBox::get();
+    unset($_SESSION['message']);
+    return $ret;
+  }
+
 }
