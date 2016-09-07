291a55f695f766796d8ab649bf3de20151f3366d
Hari Lamichhane <onwithhari@gmail.com>
2016-09-07 22:17:54 +0545

    create default home page

---
+++ Views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>I am on home page</title>
+</head>
+<body>
+ <div>
+  <form action="user/login" method="POST">
+   <fieldset>
+     <label for="username">Username</label>
+     <input type="text" name="username" value="" placeholder="s201412" />
+   </fieldset>
+   <fieldset>
+     <label for="password">Password</label>
+     <input type="password" name="password" value="" />
+   </fieldset>
+   <input type="submit" value="Login" />
+  </form>
+ </div>
+</body>
+</html>
