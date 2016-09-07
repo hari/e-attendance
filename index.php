6e3f5c6a5dd66a1d8f3a1b9205e3deaf54e35f28
Hari Lamichhane <onwithhari@gmail.com>
2016-09-07 21:37:18 +0545

    clean up

--- index.php
+++ index.php
@@ -1,38 +1,23 @@
-<?php
-require_once 'autoload.php';
-
-define('DIR_MODL', 'MODELS');
-define('DIR_VIEW', 'Views');
-define('DIR_CTRL', 'Controllers');
-define('DB_USER' , 'root');
-define('DB_PASS' , '');
-define('DB_HOST' , 'locahost');
-define('DB_NAME' , 'e_attendance');
+<?php namespace Attendance;
 
 use Attendance\Core\Route;
 
+require_once 'autoload.php';
+require_once 'helpers.php';
 $routes = require_once 'routes.php';
 
 $uri = get_current_uri();
 $current_route = get_current_route();
-
 $called = false;
+
 foreach ($routes as $route) {
   if ($current_route->is_callable($route)) {
-    $call = true;
+    $called = true;
     echo $route->call();
     break;
   }
 }
 
 if (!$called) {
-
-}
-
-function get_current_uri() {
-  return str_replace('/attendance/', '' , $_SERVER['REQUEST_URI']);
-}
-
-function get_current_route() {
-  return new Route(strtolower($_SERVER['REQUEST_METHOD']), '', '', get_current_uri());
+  throw new \Exception("Error Processing Request", 1); 
 }
