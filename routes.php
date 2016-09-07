e871b2cff750af2d30d77a4a69d5c443a294e3d4
Hari Lamichhane <onwithhari@gmail.com>
2016-09-07 21:59:24 +0545

    init project's routing

--- routes.php
+++ routes.php
@@ -5,7 +5,6 @@ use Attendance\Core\Route;
 // collection of application routes as Route object
 // all the routes will be defined here
 return [
- new Route('get', 'ApiController', 'home', ''),
- new Route('get', 'ApiController', 'test', 'web'),
- new Route('get', 'ApiController', 'do', 'web/api')
+ //index page
+ new Route('get', 'PageController', 'index', '')
 ];
