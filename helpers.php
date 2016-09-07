8af9a97a72a0cc91b65ac818a3255c511b4852e9
Hari lamichhane <hari.lamichhane@studentpartner.com>
2016-09-07 21:04:46 +0545

    collection of helper functions and definitions

---
+++ helpers.php
@@ -0,0 +1,24 @@
+<?php
+
+use Attendance\Core\Route;
+
+define('APP'     , 'Attendance');
+define('DIR_MODL', 'MODELS');
+define('DIR_VIEW', 'Views');
+define('DIR_CTRL', 'Controllers');
+define('DB_USER' , 'root');
+define('DB_PASS' , '');
+define('DB_HOST' , 'locahost');
+define('DB_NAME' , 'e_attendance');
+
+function asset($path) {
+  return "assets/$path";
+}
+
+function get_current_uri() {
+  return str_replace('/attendance/', '' , $_SERVER['REQUEST_URI']);
+}
+
+function get_current_route() {
+  return new Route(strtolower($_SERVER['REQUEST_METHOD']), '', '', get_current_uri());
+}
