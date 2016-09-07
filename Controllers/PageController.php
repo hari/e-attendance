6a16a8eb7343b9f83725e805f199869b2e0e3a7f
Hari Lamichhane <onwithhari@gmail.com>
2016-09-07 22:17:28 +0545

    add index method
    this method will show the index page according to the type of user

--- Controllers/PageController.php
+++ Controllers/PageController.php
@@ -1,1 +1,21 @@
-<?php
+<?php namespace Attendance\Controllers;
+
+use Attendance\Models\User;
+use Attendance\Core\View;
+
+class PageController {
+
+  public function index() {
+    if (!User::isLoggedIn()) 
+      return View::make('index');
+    $user = User::logged();
+    $view = ($user->role == User::STUDENT) ? 'student.index' : 
+            ($user->role == User::TEACHER) ? 'teacher.index' : 'admin.index';
+    return View::make($view, compact('user'));
+  }
+
+  public function login() {
+    return "Got request";
+  }
+
+}
