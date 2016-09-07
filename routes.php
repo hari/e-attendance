797aac69e76560429edc0505068fb0e7601a3aae
Hari Lamichhane <onwithhari@gmail.com>
2016-09-07 21:37:18 +0545

    add namespace

--- routes.php
+++ routes.php
@@ -1,6 +1,8 @@
-<?php
+<?php namespace Attendance;
 use Attendance\Core\Route;
-/** collection of application routes as Route object*/
+
+// collection of application routes as Route object
+// all the routes will be defined here
 return [
  new Route('get', 'ApiController', 'home', ''),
  new Route('get', 'ApiController', 'test', 'web'),
