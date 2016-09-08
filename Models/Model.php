8bcbc9c32205f66d3c1a4ee4f2e614b56ef732e9
Hari Lamichhane <onwithhari@gmail.com>
2016-09-08 21:36:32 +0545

    change create method to static

--- Models/Model.php
+++ Models/Model.php
@@ -5,9 +5,6 @@ abstract class Model {
   /** @var Array The property-value pair of all the attributes of model */
   protected $instance = [];
 
-  /** @var Attendance\Database\AbstractTable The table associated with this model */
-  protected $abs_table;
-
   public function getAttribute($prop) {
     if (array_key_exists($prop, $this->instance)) {
       return $this->instance[$prop];
@@ -16,12 +13,17 @@ abstract class Model {
   }
 
   /**
-   *  Creates an SQL statement from given key-value pair array and executes INSERT command
+   * A wrapper to execute for creating a new Record using AbstractTable's Insert method
+   *
+   * @param $abs_table Attendance\Database\AbstractTable
+   * @param $prop      array The key-value pair representing a new record according to table structure
+   * 
+   * @see Attendance\Database\AbstractTable
+   *
+   * @return boolean true if successfully created
    */
-  public function create($prop = []) {
-    if ($this->abs_table == null)
-      throw new \Exception("The associated table is null. Make sure to set table in constructor", 1);
-    return $this->abs_table->insert($prop);
+  public static function create($abs_table, $prop = []) {
+    return $abs_table->insert($prop);
   }
   
 }
