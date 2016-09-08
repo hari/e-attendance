32097e59fb801f60c9d06a4eec1eff39e2121c11
Hari Lamichhane <onwithhari@gmail.com>
2016-09-08 20:19:45 +0545

    add sql query for Create User Table

--- Database/UserTable.php
+++ Database/UserTable.php
@@ -1,9 +1,22 @@
 <?php namespace Attendance\Database;
 
+/**
+ * ---------------------------------------------------------------
+ * | id | name | batch | reg_no | created_by | role | created_on |
+ * ---------------------------------------------------------------
+ */
 class UserTable implements TableInterface {
 
   public static function create() {
-    
+    $sql = "CREATE TABLE IF NOT EXISTS `users` (
+           `id` INT NOT NULL AUTO_INCREMENT ,
+           `full_name` VARCHAR(50) NOT NULL ,
+           `batch` INT(4) NOT NULL ,
+           `reg_no` VARCHAR(10) NOT NULL ,
+           `created_by` INT NOT NULL ,
+           `created_on` TIMESTAMP(10) NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+           `role` INT NOT NULL ,
+           PRIMARY KEY (`id`))";
   }
 
   public static function destroy() {
