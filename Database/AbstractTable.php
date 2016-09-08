f967e62955b9530fe83025d4727659fd8116d7e5
Hari Lamichhane <onwithhari@gmail.com>
2016-09-08 21:03:26 +0545

    use abstract class
    made this class abstract to reuse different table functionalities

--- Database/AbstractTable.php
+++ Database/AbstractTable.php
@@ -2,18 +2,38 @@
 
 use Attendance\Database\Connection;
 
-abstract class TableInterface {
+abstract class AbstractTable {
 
-	public static function create();
+  protected $table_name, $error;
 
-	public static function destroy() {
+	public abstract function create();
+
+	public function destroy() {
+    return $this->execute('DROP TABLE ' . $this->getTableName());
   }
 
-  protected static function execute($sql, $params) {
+  public function getTableName() {
+    if ($this->table_name == null) 
+      throw new \Exception('Table Name is null, make sure to set table name in constructor', 1);
+    return $this->table_name;
+  }
+
+  protected function execute($sql, $params = []) {
     $con = Connection::get();
     $pst = $con->prepare($sql);
-    $pst->bindParam($params);
-    return $pst->execute();
+    if (!$pst->execute($params)) {
+      $this->setError($pst->errorInfo());
+      return false;
+    }
+    return true;
+  }
+
+  public function getError() {
+    return $this->error;
+  }
+
+  public function setError($err) {
+    $this->error = $err;
   }
 
 }
