90f9042f5b60dc1cdb03cac7721af08dfa0d5a0d
Hari Lamichhane <onwithhari@gmail.com>
2016-09-08 20:19:31 +0545

    add connection to database

--- Database/Connection.php
+++ Database/Connection.php
@@ -9,7 +9,19 @@ class Connection {
   }
 
   public static function get() {
-    
+    if (self::$connection == null) {
+      $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+      $count = 0;
+      do {
+        try {
+          $count++;
+          self::$connection = new \PDO($dsn, DB_USER, DB_PASS);
+        } catch (Exception $e) {
+          self::$connection = null;
+        }
+      } while (self::$connection == null || $count < 5);
+    }
+    return self::$connection;
   }
 
   public static function close() {
