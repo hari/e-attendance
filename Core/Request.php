2cffb91fd824699312a4762bd872ea13d1032c46
Hari Lamichhane <onwithhari@gmail.com>
2016-09-08 08:54:24 +0545

    add get and pull method
    read from the request

--- Core/Request.php
+++ Core/Request.php
@@ -1,1 +1,20 @@
 <?php namespace Attendance\Core;
+
+class Request {
+
+  public static function get($key) {
+    if (isset($_REQUEST) && isset($_REQUEST[$key])) {
+      return $_REQUEST[$key];
+    }
+    return null;
+  }
+
+  public static function pull($key) {
+    $value = self::get($key);
+    if ($value != nul) {
+      unset($_REQUEST[$key]);
+    }
+    return $value;
+  }
+
+}
