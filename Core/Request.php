3734f40168d281ec86df8ddd99045a3fc10f6e01
Hari Lamichhane <onwithhari@gmail.com>
2016-09-13 10:08:13 +0545

    Use singleton pattern

--- Core/Request.php
+++ Core/Request.php
@@ -2,19 +2,36 @@
 
 class Request {
 
-  public static function get($key) {
+  private static $instance;
+
+  private function __construct() {
+
+  }
+
+  public static function getInstance() {
+    if (self::$instance == null) {
+      self::$instance = new self;
+    }
+    return self::$instance;
+  }
+
+  public function get($key) {
     if (isset($_REQUEST) && isset($_REQUEST[$key])) {
       return $_REQUEST[$key];
     }
     return null;
   }
 
-  public static function pull($key) {
-    $value = self::get($key);
-    if ($value != nul) {
+  public function pull($key) {
+    $value = $this->get($key);
+    if ($value != null) {
       unset($_REQUEST[$key]);
     }
     return $value;
+  }
+
+  public function all() {
+    return array_merge($_POST, $_GET);
   }
 
 }
