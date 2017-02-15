9a528f726e355a5d143fc82c38b201fea42f2ece
Hari Lamichhane <onwithhari@gmail.com>
2017-02-15 17:19:39 +0545

    Modify login page.

--- Views/index.php
+++ Views/index.php
@@ -5,20 +5,20 @@
   <link rel="stylesheet" type="text/css" href="<?php echo asset('css/app.css'); ?>">
 </head>
 <body class="home">
-<?php include_once 'parts/message.php'; ?>
- <div>
-  <form class="block-form" id="login-form" action="user/login" method="POST">
-    <img id="form-logo" src="<?php echo asset('img/logo.png'); ?>" />
-    <fieldset>
-     <label for="username">Username</label>
-     <input type="text" name="username" value="" placeholder="se201412" />
-   </fieldset>
-   <fieldset>
-     <label for="password">Password</label>
-     <input type="password" name="password" value="" />
-   </fieldset>
-   <input class="btn btn-default" type="submit" value="Login" />
- </form>
-</div>
+  <?php include_once 'parts/message.php'; ?>
+  <div>
+    <form class="block-form" id="login-form" action="user/login" method="POST">
+      <img id="form-logo" src="<?php echo asset('img/logo.png'); ?>" />
+      <fieldset>
+       <label for="username">Username</label>
+       <input type="text" name="username" value="" placeholder="se201412" />
+     </fieldset>
+     <fieldset>
+       <label for="password">Password</label>
+       <input type="password" name="password" value="" />
+     </fieldset>
+     <input class="btn btn-default" type="submit" value="Login" />
+   </form>
+ </div>
 </body>
 </html>
