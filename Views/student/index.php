91f8c47d9b9b6b3383538a7b7b682aec2b2499e4
Hari Lamichhane <onwithhari@gmail.com>
2017-02-15 17:18:42 +0545

    Display student's total attendance.

--- Views/student/index.php
+++ Views/student/index.php
@@ -1,23 +1,31 @@
 <?php include_once DIR_VIEW.'./parts/header.php';?>
-<body class="teacher">
-  <?php include_once DIR_VIEW.'./parts/nav.php' ;?>
+<body class="student">
+  <?php $subjectCode = ''; if (isset($_GET['sub'])) $subjectCode = $_GET['sub'];
+  include_once DIR_VIEW.'./parts/nav.php' ;?>
   <div class="row">
-    <div class="col two"> 
-      <h1>My Attendance</h1>
-      <select onchange="" name="subject">
-        <?php if (isset($subjects) && count($subjects) > 0) : 
-        foreach($subjects as $subject) :
-          ?>
-        <option value="<?php echo $subject['id']; ?>">
-          <?php echo $subject['name']; ?>
-        </option>
-      <?php endforeach; else :?>
-      <option value="-1">No subject</option>
-    <?php endif; ?>
-  </select>
-  <h3><?php echo $absent; ?>/30</h3>
+    <h1>My Attendance</h1>
+    <p style="text-transform: uppercase;">Hey! <strong><?php echo \Attendance\Models\User::logged()->getAttribute('full_name'); ?></strong></p>
+    We are showing attendance of: 
+    <select onchange="change(this);" name="subject">
+      <?php if (isset($subjects) && count($subjects) > 0) : 
+      foreach($subjects as $subject) :
+        ?>
+      <option <?php if ($subject['code'] == $subjectCode) echo "selected"; ?> value="<?php echo $subject['code']; ?>">
+        <?php echo $subject['name']; ?>
+      </option>
+    <?php endforeach; else :?>
+    <option value="-1">No subject</option>
+  <?php endif; ?>
+</select>
+<?php
+include DIR_VIEW .'./admin/parts/a.php' ;
+$present = substr(((($total - $absent) / $total)) * 5 , 0, 4);
+?>
+<h3>You have not attended class for <?php echo $absent; ?> days out of total <?php echo $total; ?> academic days.</h3>
+<p>You have hence received <strong><?php echo $present; ?>/5</strong> in you attendance marks</p>
 </div>
-<div class="col two">
-  <h1>My Marks</h1>
-</div>
-</div>
+<script type="text/javascript">
+  function change(e) {
+    document.location.href = "?sub=" + encodeURI(e.value);
+  }
+</script>
