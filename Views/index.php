fb181f52f10a7b76522e7c4e823077efe76a1436
Hari Lamichhane <onwithhari@gmail.com>
2017-02-16 21:55:25 +0545

    Change font

--- Views/index.php
+++ Views/index.php
@@ -2,6 +2,11 @@
 <html>
 <head>
   <title>Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,inital-scale=1.0">
+  <style type="text/css">
+    <?php echo str_replace('../', asset(''), require_once 'parts/font.php'); ?>
+  </style>
   <link rel="stylesheet" type="text/css" href="<?php echo asset('css/app.css'); ?>">
 </head>
 <body class="home">
@@ -9,6 +14,7 @@
   <div>
     <form class="block-form" id="login-form" action="user/login" method="POST">
       <img id="form-logo" src="<?php echo asset('img/logo.png'); ?>" />
+      <div style="height: 2px; background-color: #efefef; margin: 16px 0;"></div>
       <fieldset>
        <label for="username">Username</label>
        <input type="text" name="username" value="" placeholder="se201412" />
@@ -17,7 +23,7 @@
        <label for="password">Password</label>
        <input type="password" name="password" value="" />
      </fieldset>
-     <input class="btn btn-default" type="submit" value="Login" />
+     <input style="margin-top: 8px;" class="btn btn-default" type="submit" value="Login" />
    </form>
  </div>
 </body>
--- Views/parts/header.php
+++ Views/parts/header.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>eAttendance System</title>
-  <link href="https://fonts.googleapis.com/css?family=Changa:300,500" rel="stylesheet"> 
+  <title>GCES E-Attendance System</title>
+  <style type="text/css">
+    <?php echo str_replace('../', asset(''), require_once 'font.php'); ?>
+  </style>
   <link rel="stylesheet" type="text/css" href="<?php echo asset('css/app.css'); ?>">
   <script src="<?php echo asset('js/lib/chart.js'); ?>"></script>
 </head>
