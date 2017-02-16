05d95fe80cbfacec244a53b4391e95af6e8806fd
Hari Lamichhane <onwithhari@gmail.com>
2017-02-16 21:55:25 +0545

    Check if the given argument has been specified

--- Core/View.php
+++ Core/View.php
@@ -1,24 +1,25 @@
 <?php namespace Attendance\Core;
 
 class View {
-  
+
   public static function make() {
     $args = func_get_args();
-    if (count($args) > 0) {
-      $file = str_replace(".", "/", $args[0]);
-      ob_start();
-      if (count($args) >= 2) {
-        extract($args[1]);
-      }
-      $file  = DIR_VIEW . '/' . $file . '.php';
-      if (!file_exists($file)) {
-        throw new \Exception("No view found '{$file}'");
-      }
-      @include_once $file;
-      $data = ob_get_contents();
-      ob_clean();
-      return $data;
+    if (count($args) == 0) {
+      return null;
     }
+    $file = str_replace(".", "/", $args[0]);
+    ob_start();
+    if (count($args) >= 2) {
+      extract($args[1]);
+    }
+    $file  = DIR_VIEW . '/' . $file . '.php';
+    if (!file_exists($file)) {
+      throw new \Exception("No view found '{$file}'");
+    }
+    @include_once $file;
+    $data = ob_get_contents();
+    ob_clean();
+    return $data;
   }
 
 }
