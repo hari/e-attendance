89b1859f62614cfc5c70fff4d021f95e656e0179
Hari Lamichhane <onwithhari@gmail.com>
2017-02-16 21:55:25 +0545

    Single font page.

---
+++ Views/parts/font.php
@@ -0,0 +1,39 @@
+<?php
+return "
+    @font-face {
+      font-family: 'Changa';
+      font-style: normal;
+      font-weight: 300;
+      src: url('../fonts/changa-v2-latin-300.eot'); /* IE9 Compat Modes */
+      src: local('Changa Light'), local('Changa-Light'),
+      url('../fonts/changa-v2-latin-300.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+      url('../fonts/changa-v2-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
+      url('../fonts/changa-v2-latin-300.woff') format('woff'), /* Modern Browsers */
+      url('../fonts/changa-v2-latin-300.ttf') format('truetype'), /* Safari, Android, iOS */
+      url('../fonts/changa-v2-latin-300.svg#Changa') format('svg'); /* Legacy iOS */
+    }
+    @font-face {
+      font-family: 'Changa';
+      font-style: normal;
+      font-weight: 400;
+      src: url('../fonts/changa-v2-latin-regular.eot'); /* IE9 Compat Modes */
+      src: local('Changa'), local('Changa-Regular'),
+      url('../fonts/changa-v2-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+      url('../fonts/changa-v2-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+      url('../fonts/changa-v2-latin-regular.woff') format('woff'), /* Modern Browsers */
+      url('../fonts/changa-v2-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+      url('../fonts/changa-v2-latin-regular.svg#Changa') format('svg'); /* Legacy iOS */
+    }
+    @font-face {
+      font-family: 'Changa';
+      font-style: normal;
+      font-weight: 700;
+      src: url('../fonts/changa-v2-latin-700.eot'); /* IE9 Compat Modes */
+      src: local('Changa Bold'), local('Changa-Bold'),
+      url('../fonts/changa-v2-latin-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+      url('../fonts/changa-v2-latin-700.woff2') format('woff2'), /* Super Modern Browsers */
+      url('../fonts/changa-v2-latin-700.woff') format('woff'), /* Modern Browsers */
+      url('../fonts/changa-v2-latin-700.ttf') format('truetype'), /* Safari, Android, iOS */
+      url('../fonts/changa-v2-latin-700.svg#Changa') format('svg'); /* Legacy iOS */
+    }
+  ";
