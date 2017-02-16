19f7379d97cb1e445cacde3b09e3f2fd774ad0bc
Hari Lamichhane <onwithhari@gmail.com>
2017-02-16 21:55:25 +0545

    New footer

---
+++ Views/parts/footer.php
@@ -0,0 +1,16 @@
+<footer style="margin-top: 16px;">
+  <div class="row">
+    <div class="col three">
+     <p>GCES E-Attendance System</p>
+   </div>
+   <div class="col three">
+     <p>Supervised by: <strong>Er. Rajendra Thapa</strong></p>
+   </div>
+   <div class="col three">
+     <p>Developed by: <strong>2014</strong> batch</p>
+     Hari Lamichhane, Shuvam Karna and Suraj Shrestha.
+   </div>
+ </div>
+</footer>
+</body>
+</html>
