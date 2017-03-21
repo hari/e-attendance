c42df602f0538b26ee112fd5854a668d06794925
Hari Lamichhane <onwithhari@gmail.com>
2017-03-21 12:49:11 +0545

    Reformat code

--- Views/parts/nav.php
+++ Views/parts/nav.php
@@ -1,28 +1,32 @@
 <?php use Attendance\Models\User; ?>
 <div class="row">
  <nav>
-   <?php
-   $user = User::logged();
-   $role = $user->getAttribute('role');
+    <?php
+    $user = User::logged();
+    $role = $user->getAttribute('role');
     //link, text, condition
-   $navs = [
-   [route('home'), 'Home', true],
-   [route('page.teacher'), 'Teachers', $role == USER::ADMIN],
-   [route('page.student'), 'Students', $role == USER::ADMIN],
-   [route('page.subject'), 'Subjects', $role == USER::ADMIN],
-   [route('page.markmodel'), 'Marks Model', $role == USER::TEACHER],
-   [route('page.mark'), 'Marks', $role == USER::TEACHER]
-   ];
-   ?>
-   <?php
-   $path = get_current_route()->getUri(true);
-   foreach ($navs as $nav) :
-     if (!$nav[2]) continue;
-     $css = "";
-     if ($path == $nav[0]) $css = ' class="selected"';
-     echo sprintf('<li%s><a href="%s">%s</a></li>', $css, $nav[0], $nav[1]);
-   endforeach;
-   ?>
+    $navs = [
+    [route('home'), 'Home', true],
+    [route('page.teacher'), 'Teachers', $role == USER::ADMIN],
+    [route('page.student'), 'Students', $role == USER::ADMIN],
+    [route('page.subject'), 'Subjects', $role == USER::ADMIN],
+    [route('page.markmodel'), 'Marks Model', $role == USER::TEACHER],
+    [route('page.mark'), 'Marks', $role == USER::TEACHER]
+    ];
+    ?>
+    <?php
+    $path = get_current_route()->getUri(true);
+    foreach ($navs as $nav) :
+        if (!$nav[2]) {
+            continue;
+        }
+        $css = "";
+        if ($path == $nav[0]) {
+            $css = ' class="selected"';
+        }
+        echo sprintf('<li%s><a href="%s">%s</a></li>', $css, $nav[0], $nav[1]);
+    endforeach;
+    ?>
    <li style="float: right">
      <a href="<?php echo route('logout'); ?>">Logout</a>
    </li>
